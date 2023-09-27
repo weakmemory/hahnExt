f232f39e27b2d9d1e633517a57425ad2221a4cba
Anton Podkopaev <podkoav239@gmail.com>
2023-09-27 17:09:05 +0200

    Initial commit

---
+++ .nix/config.nix
@@ -0,0 +1,30 @@
+{
+  ## DO NOT CHANGE THIS
+  format = "1.0.0";
+  ## unless you made an automated or manual update
+  ## to another supported format.
+
+  attribute = "hahnExt";
+
+  ## Indicate the relative location of your _CoqProject
+  ## If not specified, it defaults to "_CoqProject"
+  # coqproject = "_CoqProject";
+
+  default-bundle = "8.18";
+
+  bundles."8.16" = {
+    coqPackages.hahn.override.version = "master";
+  };
+  bundles."8.17" = {
+    coqPackages.hahn.override.version = "master";
+  };
+  bundles."8.18" = {
+    coqPackages.hahn.override.version = "master";
+  };
+
+  cachix.coq = {};
+  cachix.math-comp = {};
+  cachix.coq-community = {};
+  
+  cachix.weakmemory.authToken = "CACHIX_AUTH_TOKEN";
+}
