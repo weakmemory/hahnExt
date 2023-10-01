5ef0bbcec14aa7a9ef46605e3a1c67e5714ce85a
Anton Podkopaev <podkoav239@gmail.com>
2023-10-01 08:41:54 +0200

    Fixed compilation error in AuxRel.v

--- src/AuxRel.v
+++ src/AuxRel.v
@@ -357,8 +357,8 @@ Proof using.
   apply union_more; [done | by apply seq_restr_fwcl].
 Qed.
 
-Lemma map_rel_restr_eq_dom {A B} (f g : A -> B) s r (EQ : eq_dom s f g) :
-  restr_rel s (f ↓ r) ≡ restr_rel s (g ↓ r).
+Lemma map_rel_restr_eq_dom (f g : A -> B) rr (EQ : eq_dom s f g) :
+  restr_rel s (f ↓ rr) ≡ restr_rel s (g ↓ rr).
 Proof using.
   unfolder.
   split.
